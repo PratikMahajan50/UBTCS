cb2ab7643a95fa14717e7b201ce16a5bc3eabf80
Pratik Mahajan <pratikmahajan503@gmail.com>
2023-03-14 11:11:14 +0530

    complete
    Signed-off-by: Pratik Mahajan <pratikmahajan503@gmail.com>

--- Server/localServer.py
+++ Server/localServer.py
@@ -1,1 +1,14 @@
 # this is a localserver program 
+
+
+def initServer():
+    #Accept Nodes
+    #Accept Paths and associted weights
+    global paths,nodes
+    i=int(input("Enter Number of Nodes: "))
+    paths = [[{"src":"x"},{"des":"y"},{"w":0}]]
+    nodes = []
+    while i:
+        nodes.append(input("Enter Node Name: "))
+        i-=1
+initServer()
