eb423572e0efeee217537f6709e9fef2a2448045
Pratik Mahajan <pratikmahajan503@gmail.com>
2023-03-15 14:54:46 +0530

    updated localserver code
    Signed-off-by: Pratik Mahajan <pratikmahajan503@gmail.com>

--- Server/localServer.py
+++ Server/localServer.py
@@ -2,7 +2,7 @@
 import socket,cv2,pickle,struct,time
 import threading,os
 import numpy as np
-
+import pymongo
 #TEMP DATA
 ActiveIds = []
 
@@ -10,7 +10,7 @@ ActiveIds = []
 def initServer():
     #Accept Nodes
     #Accept Paths and associted weights
-    global paths,nodes,server_socket,port
+    global paths,nodes,server_socket,port,db,active,complete
     print("Initializing Server")
     #Demo data, will be updated by database
     nodes = ["C1","C2","C3","C4"]
@@ -39,6 +39,11 @@ def initServer():
     server_socket.bind(socket_address)
     server_socket.listen()
     print("Listening at ",socket_address)
+    print("Initializing Database")
+    client = pymongo.MongoClient("mongodb://localhost:27017")
+    db = client["ubtcs"]
+    active = db["ActiveRecords"]
+    complete = db["CompletedRecords"]
 
 
 def client(addr,client_socket):
@@ -50,10 +55,10 @@ def client(addr,client_socket):
     #Start Receiving numbers
     while(True):
         number = client_socket.recv(1024).decode()
-        if ActiveIds.index(number):
+        if active.find({"ID":number}):
             pass
         else:
-            ActiveIds.append(number)
+            active.insert({"ID":number,"src":cname})
 
 
 i=0
