8096116208433a502333c3c3ab6a3d88cee0dc4f
Pratik Mahajan <pratikmahajan503@gmail.com>
2023-03-30 11:00:30 +0530

    updated
    Signed-off-by: Pratik Mahajan <pratikmahajan503@gmail.com>

--- MainServer/database.js
+++ MainServer/database.js
@@ -1,1 +1,9 @@
 var db = connect("127.0.0.1:27017/ubtcs")
+db.VehicleDetails.insert([
+	{
+		"ID":"MH 12 AT 6666",
+		"OwnerName":"XYZ",
+		"OwnerID":"1234567890123456",
+		"Mapper Vehicle Class": "5"
+	}
+])
